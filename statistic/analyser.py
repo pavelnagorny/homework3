90110c346e45755ec6627366f8c1c35c1acc371f
pavelnagornyj <pavelnagornyj@gmail.com>
2018-08-21 15:48:40 +0300

    initial version

--- statistic/analyser.py
+++ statistic/analyser.py
@@ -5,42 +5,43 @@ Created on 13 april 2013
 @author: vl
 '''
 
+
 class Analyser(object):
     '''
     classdocs
     '''
-    
+
     def __init__(self, filepath):
         '''
         Constructor
         '''
         self.__filepath = filepath
         self.__data = []
-    
+
     def load_file(self):
         '''
         Load data from log file
         '''
-        log_file = open(self.__filepath,'r')
+        log_file = open(self.__filepath, 'r')
         data = log_file.readlines()
         log_file.close()
         for line in data:
             self.append_record(line.split())
         return self
-    
+
     def append_record(self, record):
         '''
         Append record
         '''
         self.__data.append(record)
         return self
-    
+
     def get_data(self):
         '''
         Get data
         '''
         return self.__data
-    
+
     def unique_visitors(self):
         '''
         Return array of unique visitors (ip)
@@ -58,11 +59,19 @@ class Analyser(object):
         Return array of visitors and count of visits
         '''
         # TODO: Вивести кількість всіх відвідувачів та кількість відвідувань
-        visitors_count = 0
+        visitors = []
+        for user in self.__data:
+            if user[0] not in visitors:
+                visitors.append(user[0])
+        visitors_count = len(visitors)
+
         visits_count = 0
+        for visit in self.__data:
+            visits_count += 1
         visitors = (visitors_count, visits_count)
+
         return visitors
-    
+
     def visitors_by_resources(self):
         '''
         Return dictionary of visitors and resources count
@@ -73,14 +82,15 @@ class Analyser(object):
                 visitors[record[0]] += 1
             else:
                 visitors[record[0]] = 1
-        
+
         return visitors
-    
+
     def resources_by_countries(self):
         '''
         Return dictionary of resources and coutries from which it's was loaded
         '''
         # TODO: Вивести словник ресурсів і країни з яких вони відвідувались
+
         resources = {}
+
         return resources
-        
